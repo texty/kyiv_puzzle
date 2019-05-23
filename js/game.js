a0db6fcbadded4e22d0e1e77de27afbfdba0fbd0
Vladislav <drimacus182@gmail.com>
2019-05-23 19:48:37 +0300

    робоча механіка крім фінішу

--- js/game.js
+++ js/game.js
@@ -1,47 +1,29 @@
 var source   = document.getElementById("question-card-template").innerHTML;
 var template = Handlebars.compile(source);
 
-var region = ["obolon", "nivki", "galagany", "podil", "rusanivka", "troya", "vidradny", "voskresenka", "vynogradar", "syrez"];
 var img_folder = "data/areas_one_size/";
+var img_color_folder = "data/areas_one_size/add_info/";
 
+var questions_total = 10;
 
 var scenario = window.scenario.map(function(q, i) {
     return {
-        total: 10,
+        total: questions_total,
         question_number: i + 1,
         img_path: img_folder + q.img + ".png",
+        img_color_path: img_color_folder + "color_" + q.img + ".png",
         description: q.descr,
         text: q.text,
         answers: [q.ans0, q.ans1, q.ans2, q.ans3],
-        correct: q.correct
+        correct: q.correct,
+        color: q.color,
+        next_button_text: i < questions_total - 1 ? "Наступне питання" : "Показати результат"
     }
-
 });
-
-
-// for (var i = 0; i < 10; i++) {
-//     scenario.push({
-//         total: 10,
-//         question_number: i+1,
-//         img_path: "data/areas_one_size/" + region[i] + ".png",
-//         img_path_answered: "data/areas_one_size/add_info/color_" + region[i] + ".png",
-//
-//         description: "Оболонь легко впізнати по характерній формі будинків на березі Дніпра. Таких немає у жодному іншому районі Києва",
-//         answers: [
-//             "Борщагівка",
-//             "Оболонь",
-//             "Нова Забудова (Голосіїво)",
-//             "Троєщина"
-//         ],
-//         correct: 1
-//     });
-// }
 
 var q_index = 0;
 
 var card_container = d3.select("main");
-
-
 
 document.getElementById("btn-game-start").addEventListener("click", function(){
     renderQuestion(0)
@@ -56,35 +38,47 @@ function renderQuestion(q_idx) {
         .selectAll(".answers button")
         .data([0, 1, 2, 3])
         .on("click", function(selected) {
-            if (selected === scenario[q_idx].correct) {
+            var q = scenario[q_idx];
+
+            if (selected === q.correct) {
                 // correct
 
             } else {
                 // wrong
                 d3.select(this).classed("ans-wrong", true);
-
             }
 
-            card_container.select(".description").classed("d-none", false);
+            card_container
+                .select(".description")
+                .classed("d-none", false);
 
             buttons
                 .attr("disabled", true)
-                .classed("ans-correct", ii => ii == scenario[q_idx].correct);
+                .classed("ans-correct", ii => ii == q.correct);
 
             card_container
                 .select("#btn-next-question")
                 .attr("disabled", null)
-                .on("click", function(){
-                    renderQuestion(q_idx + 1);
+                .on("click", function() {
+                    if (q_idx === questions_total - 1) renderFinish();
+                    else renderQuestion(q_idx + 1);
                 });
-        })
 
-        .each(function() {
-
-
+            if (scenario[q_idx].color) {
+                card_container
+                    .select(".img-placeholder img")
+                    .attr("src", q.img_color_path)
+            }
         });
 
     card_container.node().scrollIntoView(true);
+}
+
+function renderFinish() {
+    alert("кінець")
+
+
+
 }
 
 function offset(el) {
