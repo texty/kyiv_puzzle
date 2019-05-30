0f91ebec442e83de57d5c90a89e66ca0179e1d19
Vladislav <drimacus182@gmail.com>
2019-05-30 18:07:24 +0300

    випив багато кави. це непотрібний коміт

--- js/game.js
+++ js/game.js
@@ -160,12 +160,6 @@ function renderFinish(score, total) {
         })
 }
 
-function offset(el) {
-    var rect = el.getBoundingClientRect(),
-        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-}
 
 function preload(image_path) {
     if (document.images) {
